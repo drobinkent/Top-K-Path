f7bbe62f9418a821d7107e1b0ee627221ce2d8cf
Debobroto Das Robin <drobin@kent.edu>
2021-02-11 02:12:41 -0500

    Seeems like CP fundtions for insert and delete is okay by now

--- DistributedAlgorithms/TopKPathManager.py
+++ DistributedAlgorithms/TopKPathManager.py
@@ -53,6 +53,16 @@ class TopKPathManager:
             self.rankToPort2dMap[i] = portList
         pass
 
+    def printDS(self):
+        print("portToRankMap is :",self.portToRankMap)
+        print("rankToCounterMap is ", self.rankToCounterMap)
+        print("rankToPortAtMaxIndexMap is :",self.rankToPortAtMaxIndexMap)
+        print("portToIndexAtCurrentRankMap is ",self.portToIndexAtCurrentRankMap)
+        print("rankToPort2dMap is ")
+        for i in range (0, self.k+1):
+            print(self.rankToPort2dMap[i])
+        pass
+
     def insertPort(self, port, k):
         '''
         This function inserts the port at K'th rank
@@ -60,23 +70,33 @@ class TopKPathManager:
         :param k:
         :return:
         '''
-        oldRank = self.portToRankMap[port]
-        if((oldRank != None) and (oldRank!=k)):
+        if(k>self.k):
+            logger.error("given  rank is more than the system's available rank. so can't insert the port")
+            return False
+        if(self.rankToCounterMap[k] >= self.k):
+            print("Already k memebers in the group. Yoo may enter the port into next group")
+            return False
+        oldRank = self.portToRankMap.get(port)
+        if((oldRank != None) and (oldRank> 0) and  (oldRank!=k)):
             logger.info("Old rank of port "+str(port)+" is: "+str(oldRank)+" and new rank is: "+str(k))
             logger.info("This can not happen. Please Debug. Exiting the thread!!!!")
             exit(1)
+        if((oldRank != None) and (oldRank> 0) and  (oldRank==k)):
+            logger.info("The port is already in rank-"+str(k))
+            print("The port is already in rank-",k)
+            return True
         self.portToRankMap[port] = k
         self.rankToCounterMap[k] = self.rankToCounterMap[k] + 1
         self.rankToPortAtMaxIndexMap[k] = port
-        oldIndex = self.portToIndexAtCurrentRankMap[port]
-        if((oldIndex != None) and (oldIndex!=self.rankToCounterMap[k])):
+        oldIndex = self.portToIndexAtCurrentRankMap.get(port)
+        if((oldIndex != None) and (oldIndex >0) and (oldIndex!=self.rankToCounterMap[k])):
             logger.info("Old index of port "+str(port)+" is: "+str(oldIndex)+" and new index is: "+str(self.rankToCounterMap[k]))
             logger.info("This can not happen. Please Debug. Exiting the thread!!!!")
             exit(1)
-        self.portToIndexAtCurrentRankMap[port] = self.rankToPortAtMaxIndexMap[k]
+        self.portToIndexAtCurrentRankMap[port] = self.rankToCounterMap[k]
         self.rankToPort2dMap[k][self.rankToCounterMap[k]] = port
         #next we need to build the control message
-        pass
+        return True
 
     def deletePort(self, port):
         '''
@@ -86,22 +106,61 @@ class TopKPathManager:
         :return:
         '''
 
+
+
         oldRank = self.portToRankMap[port]
         if(oldRank == None):
             logger.info("Old rank of port to be deleted("+str(port)+") is None. If you are deleting a port before inserting it then you are ok. otherwise There is some bug.")
-            logger.info("To ensure sonsistency we are exiting.")
+            logger.info("To ensure consistency we are exiting.")
             exit(1)
-        self.portToRankMap[port] = 0
-        self.rankToCounterMap[oldRank] = self.rankToCounterMap[oldRank] - 1
-        portAtMaxIndex = self.rankToPortAtMaxIndexMap[oldRank]
-        oldIndex = self.portToIndexAtCurrentRankMap[port]
+            oldIndex = self.portToIndexAtCurrentRankMap[port]
         if((oldIndex == None) ):
             logger.info("Old index of port "+str(port)+" to be deleted is None. This means the port is already not existing ")
             logger.info("This can not happen. Please Debug. Exiting the thread!!!!")
             exit(1)
+        self.portToRankMap[port] = 0
+        self.rankToCounterMap[oldRank] = self.rankToCounterMap[oldRank] - 1
+        portAtMaxIndex = self.rankToPortAtMaxIndexMap.get(oldRank)
+        newMaxIndexOfTheRank = self.portToIndexAtCurrentRankMap.get(portAtMaxIndex)-1
         self.portToIndexAtCurrentRankMap[port] = 0
         self.rankToPort2dMap[oldRank][oldIndex] = portAtMaxIndex
-        self.portToIndexAtCurrentRankMap[portAtMaxIndex] = oldIndex  #This is necessary because we are shifting it's location
+        #TODO special case when we delete a port from a group. the port itself was the last port in the group. Need s[ecial test for that
+        if(port == self.rankToPortAtMaxIndexMap[oldRank]):
+            self.portToRankMap[port] = 0
+            self.portToIndexAtCurrentRankMap[portAtMaxIndex] = 0  #This is necessary because we are shifting it's location
+        else:
+            self.portToIndexAtCurrentRankMap[portAtMaxIndex] = oldIndex  #This is necessary because we are shifting it's location
+        self.rankToPortAtMaxIndexMap[oldRank] = self.rankToPort2dMap[oldRank][newMaxIndexOfTheRank]
         #next we need to build the control message
+
         pass
 
+
+def testDriverFunction():
+    mgr = TopKPathManager(dev = None, k=8)
+    mgr.insertPort(port = 4, k =1)
+    mgr.printDS()
+    mgr.insertPort(port = 5, k =1)
+    mgr.insertPort(port = 6, k =1)
+    mgr.printDS()
+    mgr.deletePort(port = 5)
+    mgr.insertPort(port = 7, k =1)
+    mgr.insertPort(port = 8, k =1)
+    mgr.insertPort(port = 9, k =1)
+    mgr.insertPort(port = 10, k =1)
+    mgr.insertPort(port = 11, k =1)
+    mgr.insertPort(port = 12, k =1)
+    mgr.insertPort(port = 13, k =2)
+    mgr.deletePort(port = 7)
+    mgr.insertPort(port = 14, k =8)
+    mgr.deletePort(port = 12)
+    mgr.deletePort(port = 13)
+    mgr.insertPort(port = 23, k =2)
+    mgr.insertPort(port = 13, k =2)
+    mgr.deletePort(port = 8)
+    mgr.deletePort(port = 4)
+    mgr.insertPort(port=4, k = 5)
+    mgr.insertPort(port=4, k = 5)
+    mgr.printDS()
+
+testDriverFunction()
